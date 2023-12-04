039021e4a244dbd816144b66063512033a83c4f4
humorW <yawu11@s.sfusd.edu>
2023-12-04 15:36:17 -0800

    add the Level 3

--- Scripts/level_definitions.gd
+++ Scripts/level_definitions.gd
@@ -14,7 +14,15 @@ var level_2 = [
 	[1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-var levels = [level_1, level_2]
+var level_3 = [
+	[5, 4, 5, 4, 5, 4, 5, 4, 5],
+	[4, 3, 4, 3, 4, 3, 4, 3, 4],
+	[3, 2, 3, 2, 3, 2, 3, 2, 3],
+	[2, 1, 2, 1, 2, 1, 2, 1, 2],
+	[1, 0, 1, 0, 1, 0, 1, 0, 1]
+]
+
+var levels = [level_1, level_2, level_3]
 
 func get_current_level():
 	return levels[current_level - 1]
